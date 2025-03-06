d115c5d9fdbc80b15335fa1b96c9962afa70a9dd
Giulio Dalla Riva <giuliodr@spc.int>
2025-03-06 14:03:59 +1100

    almost good to go, missing unit of measures

--- src/data.R
+++ src/data.R
@@ -45,11 +45,58 @@ all_df <- main_dsd_sources |>
     .id = "source"
   )
 
-all_df |>
+full_df <- all_df |>
+  # fill in missing dimension as _T dimensions
+  # we don't remove NA from season yet, as we want
+  # to distinguish annual and quarterly data
   mutate(
-  season = replace_na(season,"_T"),
-  area = replace_na(area,"_T"),
-  fishery = replace_na(fishery,"_T"),
-  age = replace_na(age,"_T"),
-  stage = replace_na(stage,"_T"),
+    area = replace_na(area,"_T"),
+    fishery = replace_na(fishery,"_T"),
+    age = replace_na(age,"_T"),
+    stage = replace_na(stage,"_T"),
+  ) |>
+  # input frequency of observation (annual if not seasonal)
+  # and write time period in SDMX style (2024, 2025-1)
+  mutate(
+    YEAR_MODEL = "2023",
+    SPECIES = "YFT",
+    FREQ = if_else(is.na(season), "A", "Q"),
+    TIME_PERIOD = if_else(
+      FREQ == "Q",
+      paste(as.character(year),season,sep = "-"),
+      as.character(year)
+    ),
+    AREA = paste0("2023_YFT_", area)
+  ) |>
+  # capitalise names where necessary
+  rename(
+    FISHERY := fishery,
+    AGE := age,
+    STAGE := stage,
+  ) |>
+  # input observation attributes
+  mutate(
+    UNIT_MEASURE = case_when(
+
+      indicator == "f" ~ "RATIO",
+
+    ),
+    COMMENT = "",
+    OBS_STATUS = "",
+  )
+
+DF_FISH_CATCH <- full_df |>
+  select(
+    YEAR_MODEL,
+    SPECIES,
+    FREQ,
+    TIME_PERIOD,
+    AREA,
+    FISHERY,
+    INDICATOR,
+    OBS_VALUE,
+    UNIT_MEASURE,
+    COMMENT,
+    OBS_STATUS,
+
   )
