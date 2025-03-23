8791afd024546b9334733008168a83c5fabbc6eb
Giulio Dalla Riva <giuliodr@spc.int>
2025-03-24 07:47:19 +1100

    BET adapt

--- src/structure_helpers.R
+++ src/structure_helpers.R
@@ -17,9 +17,9 @@ DF_FISH_CATCH <- read_delim("data/DF_FISH_CATCH_2023_BET.csv", delim = ";")
 DF_FISH_CATCH |>
   select(FISHERY) |>
   unique() |>
-  filter(FISHERY |> str_starts("2023_YFT")) |>
+  filter(FISHERY |> str_starts("2023_BET")) |>
   mutate(
     name = paste("Fishery", str_extract(FISHERY,"[^_]+$")),
-    parent_id = "2023_YFT"
+    parent_id = "2023_BET"
   ) |>
-  write_csv("structs/Fishery_cl.csv", append = TRUE)
+  write_csv("structs/Fishery_cl_bet.csv", append = TRUE)
